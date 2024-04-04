5cc2d30b2b794e8875739e58f3697c030f78a97d
Davide Cavion <davide@rfnm.io>
2024-04-04 19:15:01 +0200

    Dual DMA master read streaming

--- main.c
+++ main.c
@@ -15,24 +15,6 @@
 
 #include "fsl_common.h"
 #include "rfnm_m7.h"
-/*******************************************************************************
- * Definitions
- ******************************************************************************/
-#define DEMO_GPT_IRQn GPT1_IRQn
-/* GPT channel used for input capture */
-#define BOARD_GPT_INPUT_CAPTURE_CHANNEL kGPT_InputCapture_Channel2
-/* Interrupt to enable and flag to read; depends on the GPT channel used */
-#define EXAMPLE_GPT_CAPTURE_IRQHandler     GPT1_IRQHandler
-#//define BOARD_GPT_CHANNEL_INTERRUPT_ENABLE kGPT_InputCapture2InterruptEnable
-//#define BOARD_GPT_CHANNEL_FLAG             kGPT_InputCapture2Flag
-
-/*******************************************************************************
- * Prototypes
- ******************************************************************************/
-
-/*******************************************************************************
- * Variables
- ******************************************************************************/
 
 volatile bool gptIsrFlag = false;
 
@@ -43,7 +25,7 @@ int pp_txrx = 0;
 
 rf_ctrl_s rf_ctrl;
 
-void EXAMPLE_GPT_CAPTURE_IRQHandler(void)
+void GPT1_IRQHandler(void)
 {
     /* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F, Cortex-M7, Cortex-M7F Store immediate overlapping
       exception return operation might vector to incorrect interrupt */
@@ -66,7 +48,7 @@ void EXAMPLE_GPT_CAPTURE_IRQHandler(void)
             cap_to_trigger += UINT32_MAX;
         }
 
-        gpt_time_at_cap = GPT_GetInputCaptureValue(GPT1, BOARD_GPT_INPUT_CAPTURE_CHANNEL);
+        gpt_time_at_cap = GPT_GetInputCaptureValue(GPT1, kGPT_InputCapture_Channel2);
 
         gpt_trigger_time = gpt_time_at_cap + (cap_to_trigger / 16);
         gpt_trigger_time -= INTERRUPT_LATENCY_OFFSET;
@@ -133,7 +115,6 @@ void EXAMPLE_GPT_CAPTURE_IRQHandler(void)
 
 
 
-
 int main(void)
 {
     uint32_t captureVal = 0;
@@ -176,15 +157,239 @@ int main(void)
 
     GPT_StartTimer(GPT1);    
 #ifndef RFNM_CHECK_ALIGNMENT
-    GPT_SetInputOperationMode(GPT1, BOARD_GPT_INPUT_CAPTURE_CHANNEL, kGPT_InputOperation_BothEdge);
+    GPT_SetInputOperationMode(GPT1, kGPT_InputCapture_Channel2, kGPT_InputOperation_BothEdge);
 #else
-    GPT_SetInputOperationMode(GPT1, BOARD_GPT_INPUT_CAPTURE_CHANNEL, kGPT_InputOperation_RiseEdge);
+    GPT_SetInputOperationMode(GPT1, kGPT_InputCapture_Channel2, kGPT_InputOperation_RiseEdge);
 #endif
     
     rfnm_gpio_init();
 
 //    gpio_pin_config_t led_config = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
 //    GPIO_PinInit(GPIO4, 30U, &led_config);
+
+
+
+// transfer size -> MBps single ch -> MBps single ch with partial reset as you are writing -> MBps with dual channel
+
+// 0x400 -> 334 -> 360 -> 676 MBps
+// 0x800 -> 458 -> 492 -> 787 MBps
+// 0x1000 -> 588 -> 605
+// 0x2000 -> 669 -> 684
+// 0x4000 -> 730 -> 737 -> 780 MBps
+
+
+    uint32_t *reg;
+    volatile int cnt = 0;
+
+    reg = DMA_READ_ENGINE_EN_OFF; 
+    *reg = 0x0;
+
+    reg = DMA_READ_ENGINE_EN_OFF; 
+    *reg = 0x1;
+    reg = DMA_READ_INT_MASK_OFF; 
+    *reg = 0x0;
+    
+    uint32_t can_setup_dma[2] = {1, 1};
+
+    volatile struct rfnm_bufdesc_rx *bufdesc = (struct rfnm_bufdesc_rx *) VSPA_MEM_ADDR_FROM_M7;
+    volatile struct rfnm_la9310_status *rfnm_la9310_status = (struct rfnm_la9310_status *) 0x00900000;
+    volatile struct rfnm_m7_status *rfnm_m7_status = (struct rfnm_la9310_status *) (0x00900000 + 0x100);
+    //volatile struct rfnm_rx_usb_head *rfnm_rx_usb_head = (struct rfnm_la9310_status *) 0x96400000;
+    
+    
+    int32_t dma_buf_last_map[2] = {-1, -1};
+
+    int16_t buffs_to_ign[RFNM_IGN_BUFCNT];
+    int16_t last_ign_buf = 0;
+
+    for(int i = 0; i < RFNM_IGN_BUFCNT; i++) {
+        buffs_to_ign[i] = -1;
+    }
+
+    rfnm_la9310_status[0].age = 0;
+    while(rfnm_la9310_status[0].age == 0) {}
+
+    rfnm_m7_status->rx_head = 0;
+
+    
+    
+
+
+    while(1) {
+
+        for(int dma = 0; dma < 2; dma++) {
+            uint32_t off = 0x200 * dma;
+
+            reg = DMA_CH_CONTROL1_OFF_RDCH_0 + off;
+            //while((*reg & 0x60) != 0x60) { }
+            if((*reg & 0x60) == 0x60) {
+                can_setup_dma[dma] = 1;
+            }
+            
+            if(!can_setup_dma[dma]) {
+                continue;
+            }
+
+            if(dma_buf_last_map[dma] != -1) {
+                bufdesc[dma_buf_last_map[dma]].read = 1;
+                dma_buf_last_map[dma] = -1;
+            }
+
+            
+
+            uint16_t ages[4];
+            
+            for(int i = 0; i < 4; i++) {
+                ages[i] = rfnm_la9310_status[i].age;
+            }
+
+            int is_ages_overflowing = 0;
+            
+            for(int i = 1; i < 4; i++) {
+                if(ages[0] > ages[i] && (ages[0] - ages[i]) > INT16_MAX) {
+                    is_ages_overflowing = 1;
+                    break;
+                } else if(ages[i] > ages[0] && (ages[i] - ages[0]) > INT16_MAX) {
+                    is_ages_overflowing = 1;
+                    break;
+                }
+            }
+
+            int winning_age_val = ages[0];
+            int winning_age_id = 0;
+            for(int i = 1; i < 4; i++) {
+                if(
+                    (is_ages_overflowing && winning_age_val >= ages[i]) || 
+                    (!is_ages_overflowing && winning_age_val <= ages[i])
+                ) {
+                    winning_age_id = i;
+                    winning_age_val = ages[i];
+                }
+            }
+
+//            if(winning_age_id == 0 && skip[0]) {
+//                continue;
+//            }
+
+            rfnm_m7_status->tx_buf_id = rfnm_la9310_status[winning_age_id].tx_buf_id;
+
+
+            int win_b = -1;
+            int win_age = 0;
+
+            for(int b = 0; b < RFNM_RX_BUF_CNT; b++) {
+                uint16_t t_age = rfnm_la9310_status[winning_age_id].rx_buf[b];
+                if(0xffff != t_age && t_age > win_age) {
+                    int must_ignore = 0;
+                    for(int i = 0; i < RFNM_IGN_BUFCNT; i++) {
+                        if(b == buffs_to_ign[i]) {
+                            must_ignore = 1;
+                        }
+                    }
+                    if(!must_ignore) {
+                        win_b = b;
+                        win_age = t_age;
+                    }
+                    
+                    
+                }
+            }
+
+#if 0
+            int read_b = -1;
+            for(int b = 0; b < RFNM_RX_BUF_CNT; b++) {
+                if(read_b >= 0) {
+                    if(rfnm_buf_age[b] > rfnm_buf_age[read_b]) {
+                        read_b = b;
+                    }
+                } else {
+                    if(rfnm_buf_age[b] > 0) {
+                        read_b = b;
+                    }
+                }
+            }
+#endif
+
+            if(win_b < 0) {
+                continue;
+            }
+
+            
+            reg = DMA_CH_CONTROL1_OFF_RDCH_0 + off; 
+            //*reg = 0x04000008;
+            *reg = 0x00000008 ;//| (1<<25);
+
+            reg = DMA_READ_INT_CLEAR_OFF;
+            *reg = 0x1;
+
+            reg = DMA_TRANSFER_SIZE_OFF_RDCH_0 + off; 
+            //*reg =  0x40000;
+            *reg =  sizeof(struct rfnm_bufdesc_rx);
+            reg = DMA_SAR_LOW_OFF_RDCH_0 + off; 
+            //*reg = 0x1C010A00;
+            *reg =  0x1F400000 + (win_b * sizeof(struct rfnm_bufdesc_rx)); 
+            //*reg =  0x1c000000;
+            //*reg = 0x1f000000;
+            reg = DMA_SAR_HIGH_OFF_RDCH_0 + off; 
+            *reg =  0x00000000;
+            reg = DMA_DAR_LOW_OFF_RDCH_0 + off; 
+            *reg =  0x96400000 + (rfnm_m7_status->rx_head * sizeof(struct rfnm_bufdesc_rx));
+            reg = DMA_DAR_HIGH_OFF_RDCH_0 + off; 
+            *reg =  0x00000000;
+
+            
+            reg = DMA_READ_DOORBELL_OFF; 
+            *reg =  dma;
+
+            can_setup_dma[dma] = 0;
+            dma_buf_last_map[dma] = win_b;
+
+            buffs_to_ign[last_ign_buf] = win_b;
+
+            if(++last_ign_buf == RFNM_IGN_BUFCNT) {
+                last_ign_buf = 0;
+            }
+
+            //rfnm_buf_age[(winning_age_id * 32) + win_b] = 0xffff;
+
+            //rfnm_buf_age[read_b] = 0; // meh what a shitshow
+            //rfnm_buf_age[(win_b * 32)] = 0xffff; // meh what a shitshow
+
+            cnt++;
+
+            if(++rfnm_m7_status->rx_head == RFNM_ADC_BUFCNT) {
+                rfnm_m7_status->rx_head = 0;
+            }
+        }
+
+        
+
+        
+
+        
+
+        
+
+        
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
      
     memset(&m7_dgb, 0, sizeof(struct rfnm_m7_dgb));
@@ -204,7 +409,7 @@ int main(void)
             rfnm_fe_generic_init(&m7_dgb);
 
             GPT_EnableInterrupts(GPT1, kGPT_InputCapture2InterruptEnable);
-            EnableIRQ(DEMO_GPT_IRQn);
+            EnableIRQ(GPT1_IRQn);
             GPT_StartTimer(GPT1);
 
             m7_dgb.m7_tdd_initialized = 1;
@@ -220,7 +425,7 @@ int main(void)
         /* Check whether occur interupt */
         if (true == gptIsrFlag)
         {
-            captureVal = GPT_GetInputCaptureValue(GPT1, BOARD_GPT_INPUT_CAPTURE_CHANNEL);
+            captureVal = GPT_GetInputCaptureValue(GPT1, kGPT_InputCapture_Channel2);
             //PRINTF("\r\n Capture value =%x\r\n", captureVal);
             gptIsrFlag = false;
         }
--- rfnm_m7.h
+++ rfnm_m7.h
@@ -4,8 +4,20 @@
 #define PCIE_CTRL_ADDR_OFFSET (0x1C00F740 - 0x18000000)
 #define PCIE_CTRL_ADDR_FROM_M7 (0xD0000000 + PCIE_CTRL_ADDR_OFFSET) // D400 F740
 #define PCIE_GPIO_ADDR_FROM_M7 (0xD0000000 + 0x2300000)
+#define VSPA_MEM_ADDR_OFFSET (0x1F400000 - 0x18000000)
+#define VSPA_MEM_ADDR_FROM_M7 (0xD0000000 + VSPA_MEM_ADDR_OFFSET)
 
 #define INTERRUPT_LATENCY_OFFSET (5.5 * 3.84)
+
+
+typedef uint32_t vspa_complex_fixed16;
+#define FFT_SIZE 512
+
+#define RFNM_RX_BUF_CNT 7
+#define RFNM_IGN_BUFCNT 3
+#define ERROR_MAX 0x9
+
+#define RFNM_ADC_BUFCNT (4096*4) // 4096 ~= 10ms
 
 
 typedef struct {
@@ -44,10 +56,87 @@ RFNM_PACKED_STRUCT(
 	} 
 ); 
 
+struct rfnm_bufdesc_tx {
+	vspa_complex_fixed16 buf[FFT_SIZE];
+	uint32_t dac_id;
+	uint32_t phytimer;
+	uint32_t cc;
+	uint32_t axiq_done;
+	uint32_t iqcomp_done;
+	// (64 - (4 * 5)) / 4 = 22
+	uint32_t pad_to_64[11];
+};
+
+struct rfnm_bufdesc_rx {
+	vspa_complex_fixed16 buf[FFT_SIZE];
+	uint32_t adc_id;
+	uint32_t phytimer;
+	uint32_t cc;
+	uint32_t axiq_done;
+	uint32_t iqcomp_done;
+	uint32_t read;
+	// (64 - (4 * 5)) / 4 = 22
+	uint32_t pad_to_64[10];
+};
+
+struct rfnm_la9310_status {
+	uint16_t rx_buf[8]; //use 8 to keep align, rather than RFNM_RX_BUF_CNT
+	uint32_t age;
+	uint32_t tx_buf_id;
+	uint32_t g_errors[ERROR_MAX];
+    uint32_t pad_to_64[1];
+};
+
+RFNM_PACKED_STRUCT(
+	struct rfnm_m7_status {
+		uint32_t tx_buf_id;
+		uint32_t rx_head;
+	} 
+); 
+/*
+RFNM_PACKED_STRUCT(
+    struct rfnm_rx_usb_head {
+        uint32_t magic;
+		uint32_t phytimer;
+		uint32_t dropped;
+		uint32_t adc_cc;
+		uint32_t rx_cc;
+		uint32_t adc_id;
+    }
+);
+*/
+
 
 void rfnm_fe_load_latches(struct rfnm_m7_dgb * dgb_dt, int txrx);
 void rfnm_fe_trigger_latches(struct rfnm_m7_dgb * dgb_dt, int txrx);
 void rfnm_fe_manual_clock(int dgb_id, int id);
 int rfnm_fe_generic_init(struct rfnm_m7_dgb * dgb_dt);
+
+
+
+#define PCIE_DMA_BASE (0x33800000 + 0x0)
+
+#define DMA_READ_ENGINE_EN_OFF          (PCIE_DMA_BASE + 0x38002C)
+#define DMA_READ_INT_MASK_OFF           (PCIE_DMA_BASE + 0x3800A8)
+#define DMA_CH_CONTROL1_OFF_RDCH_0      (PCIE_DMA_BASE + 0x380300)
+
+#define DMA_TRANSFER_SIZE_OFF_RDCH_0    (PCIE_DMA_BASE + 0x380308)
+#define DMA_SAR_LOW_OFF_RDCH_0          (PCIE_DMA_BASE + 0x38030C)
+#define DMA_SAR_HIGH_OFF_RDCH_0         (PCIE_DMA_BASE + 0x380310)
+#define DMA_DAR_LOW_OFF_RDCH_0          (PCIE_DMA_BASE + 0x380314)
+#define DMA_DAR_HIGH_OFF_RDCH_0         (PCIE_DMA_BASE + 0x380318)
+
+#define DMA_READ_DOORBELL_OFF           (PCIE_DMA_BASE + 0x380030)
+
+#define DMA_READ_INT_CLEAR_OFF          (PCIE_DMA_BASE + 0x3800AC)
+
+
+
+
+
+
+
+
+
 
 #endif
